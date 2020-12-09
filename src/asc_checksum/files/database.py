52179c67febda1d84314fea22c7136563772755c
Luca1152 <t.luca1152@gmail.com>
2020-12-09 11:10:10 +0200

    Fix adding directories logic and add check function
    Adding an entire directory in the database and keeping track only of one SHA256 turned out to be a bad idea, as changing a directory's contents and changing them back again wouldn't result in the same hash as before. This may be due to the order the files are recursively parsed.
    
    Now, when adding a directory, its files are added instead, with a SHA256 for each file. This won't detect new foreign files, but it doesn't seem that useful do detect this either.
    
    Also, now there's a function to check for the integrity of all the files in the database.

--- src/asc_checksum/files/database.py
+++ src/asc_checksum/files/database.py
@@ -1,4 +1,7 @@
+import glob
 import json
+import os
+import pathlib
 
 from src.asc_checksum.definitions import DATABASE_DIR
 from src.asc_checksum.scripts.checksum import checksum
@@ -11,28 +14,75 @@ class Database:
         self.load()
 
     def load(self):
-        """Load the files-to-checksum database into memory"""
+        """Load the database of files and their SHA256s into memory"""
 
         try:
             self._database = json.loads(DATABASE_DIR.read_text(encoding="utf-8"))
         except FileNotFoundError:
             self._database = dict()
 
-    def add(self, path):
-        """Add a path (file or directory) in the database to keep track of its hash"""
-
-        if path in self._database:
-            print(f"The path '{path}' is already in the database")
+    def _add_file(self, file_path):
+        if file_path in self._database:
+            print(f"The path '{file_path}' already is in the database (use 'update' to overwrite)")
         else:
-            self._database[path] = checksum(path)
+            self._database[file_path] = checksum(file_path)
+
+    @staticmethod
+    def _get_files_from_directory(path):
+        # Remove '/' from the end of the directory, if it has one
+        path = pathlib.Path(path)
+
+        # Get all files from the directory
+        os.chdir(path)
+        files = [f"{path}/{file}" for file in glob.glob("**", recursive=True) if os.path.isfile(file)]
+
+        return files
+
+    def add(self, path):
+        """
+        Add a path in the database to keep track of its hash.
+        If it's a directory, its files will be added instead.
+        """
+
+        # The path is a file
+        if os.path.isfile(path):
+            self._add_file(path)
+
+        # The path is a directory
+        else:
+            files = self._get_files_from_directory(path)
+            for file in files:
+                self._add_file(file)
+
+    def check_integrity(self):
+        """Check whether all the files from the database still have the same SHA256"""
+
+        for path, old_hash in self._database.items():
+            new_hash = checksum(path)
+            if new_hash != old_hash:
+                return False
+        return True
 
     def remove(self, path):
-        """Remove a path (file or directory) from the database"""
+        """
+        Remove a path from the database.
+        If it's a directory, its files will be removed instead, if found in the database.
+        """
 
-        if path in self._database:
-            self._database.pop(path)
+        # The path a file
+        if os.path.isfile(path):
+            if path in self._database:
+                self._database.pop(path)
+                print(f"The file path '{path}' was removed from the database")
+            else:
+                print(f"The file path '{path}' was not found in the database")
+
+        # The path is a directory
         else:
-            print(f"The path '{path}' was not found in the database")
+            files = self._get_files_from_directory(path)
+            for file in files:
+                self._database.pop(file)
+            print(f"The directory path '{path}' was removed from the database")
 
     def save(self):
         """Write the entire database to a JSON file"""
--- src/asc_checksum/scripts/checksum.py
+++ src/asc_checksum/scripts/checksum.py
@@ -1,24 +1,11 @@
 import hashlib
-import os
 
 
-def _digest_file(path, hash_object):
-    with open(path, "rb") as file:
-        while file_chunk := file.read(8192):
-            hash_object.update(file_chunk)
-
-
-def _digest(path, hash_object):
-    if os.path.isdir(path):
-        for file_name in os.listdir(path):
-            _digest(f"{path}/{file_name}", hash_object)
-    else:
-        _digest_file(path, hash_object)
-
-
-def checksum(path):
-    """Return the SHA256 of a file or a directory's content"""
+def checksum(file_path):
+    """Return the SHA256 of a file"""
 
     sha256_object = hashlib.sha256()
-    _digest(path, sha256_object)
+    with open(file_path, "rb") as file:
+        while file_chunk := file.read(8192):
+            sha256_object.update(file_chunk)
     return sha256_object.hexdigest()
